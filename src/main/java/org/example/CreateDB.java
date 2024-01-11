8f4fb8de7062ae201b94bad1056aa2abbb325144
Jenny Storm <jenny.storm@gmail.com>
2024-01-11 16:57:53 +0100

    Uppdaterat databaser för att passa projektet

--- src/main/java/org/example/CreateDB.java
+++ src/main/java/org/example/CreateDB.java
@@ -3,11 +3,13 @@ import java.sql.*;
 
 public class CreateDB {
     // Anger namn på databasen
-    static String filename = "test.db";
+    static String filename = "bookingsystem.db";
     static String url = "jdbc:sqlite:src/main/java/org/example/" + filename;
     public static void main(String[] args) {
         CreateNewDB();
-        CreateTable();
+        CreateTableTables();
+        CreateTableCustomers();
+        CreateTableBookings();
     }
     // Testar att ansluta till db filen, om ingen fil finns så skapas en ny automatiskt
     static void CreateNewDB() {
@@ -20,18 +22,63 @@ public class CreateDB {
         }
     }
     // Skapar en ny tabell i databasen
-    static void CreateTable() {
+    static void CreateTableTables() {
         // SQL koden som skickas till databasen
-        // Ändra till värden som passar uppgiften
-        String query = "CREATE TABLE IF NOT EXISTS test_table"
-                + "(id integer PRIMARY KEY, "
-                + "	name text NOT NULL, "
-                + "number integer);";
+        // Bords-tabell
+        String query = "CREATE TABLE IF NOT EXISTS Tables"
+                + "(TableID INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + "TableNumber INTEGER,"
+                + "Seats INTEGER );";
+
         try {
             Connection kontakt = DriverManager.getConnection(url); // Skapar en ny koppling till filen vid urlen
             Statement cursor = kontakt.createStatement(); // Typ samma som cursor i python
             cursor.execute(query); // Skickar sql queryt till databasen
-            System.out.println("Tabell skapad");
+            System.out.println("Table-tabell skapad");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    // Skapar en ny tabell i databasen
+    static void CreateTableCustomers() {
+        // SQL koden som skickas till databasen
+        // Kund-tabell
+        String query = "CREATE TABLE IF NOT EXISTS Customers"
+                + "(CustomerID INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + "Name TEXT,"
+                + "Email TEXT,"
+                + "Phone TEXT);";
+
+        try {
+            Connection kontakt = DriverManager.getConnection(url); // Skapar en ny koppling till filen vid urlen
+            Statement cursor = kontakt.createStatement(); // Typ samma som cursor i python
+            cursor.execute(query); // Skickar sql queryt till databasen
+            System.out.println("Customer-tabell skapad");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    // Skapar en ny tabell i databasen
+    static void CreateTableBookings() {
+        // SQL koden som skickas till databasen
+        // Boknings-tabell
+        String query = "CREATE TABLE IF NOT EXISTS Bookings"
+                + "(BookingID INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + "CustomerID INTEGER,"
+                + "TableID INTEGER,"
+                + "Day TEXT,"
+                + "Time TEXT,"
+                + "FOREIGN KEY (CustomerID)"
+                + "REFERENCES Customers (CustomerID)"
+                + "FOREIGN KEY (TableID)"
+                + "REFERENCES Tables (TableID));";
+
+        try {
+            Connection kontakt = DriverManager.getConnection(url); // Skapar en ny koppling till filen vid urlen
+            Statement cursor = kontakt.createStatement(); // Typ samma som cursor i python
+            cursor.execute(query); // Skickar sql queryt till databasen
+            System.out.println("Booking-tabell skapad");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
--- src/main/java/org/example/ReadDB.java
+++ src/main/java/org/example/ReadDB.java
@@ -13,16 +13,24 @@ public class ReadDB {
     public static void main (String[] args){
         ReadDB read = new ReadDB();
 
-        // Printar ut innehållet i databasen
-        for (String i : read.selectAll()) {
+        // Printar ut innehållet i tabellen Tables
+        for (String i : read.selectAllFromTableTables()) {
+            System.out.println(i);
+        }
+        // Printar ut innehållet i tabellen Customers
+        for (String i: read.selectAllFromTableCustomers()) {
+            System.out.println(i);
+        }
+        // Printar ut alla bokningar i datumordning
+        for (String i: read.selectAllBookingsSortedByDate()) {
             System.out.println(i);
         }
     }
     // Skickar ett SELECT query till databasen
     // Returnerar en Arraylist med värdena i test tabellen
     // Ändra till flera metoder så man kan kolla bokningar, namn osv
-    public ArrayList<String> selectAll() {
-        String query = "SELECT id, name, number FROM test_table";
+    public ArrayList<String> selectAllFromTableTables() {
+        String query = "SELECT TableID, TableNumber, Seats FROM Tables;";
         ArrayList<String> lst_response = new ArrayList<String>();
 
         try {
@@ -30,13 +38,64 @@ public class ReadDB {
             Statement cursor = kontakt.createStatement();
             ResultSet r = cursor.executeQuery(query);
 
-            // Lägger till resultatet från databsen i en arraylist och returnerar den
+            // Lägger till resultatet från databasen i en arraylist och returnerar den
             while (r.next()) {
-                String id = String.valueOf(r.getInt("id"));
-                String name = r.getString("name");
-                String number = String.valueOf(r.getInt("number"));
+                String id = String.valueOf(r.getInt("TableID"));
+                String number = String.valueOf(r.getInt("TableNumber"));
+                String seats = String.valueOf(r.getInt("Seats"));
 
-                lst_response.add(id + "\t" + name + "\t" + number);
+                lst_response.add(id + "\t" + number + "\t" + seats);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return lst_response;
+    }
+
+    public ArrayList<String> selectAllFromTableCustomers() {
+        String query = "SELECT CustomerID, Name, Email, Phone FROM Customers;";
+        ArrayList<String> lst_response = new ArrayList<String>();
+
+        try {
+            Connection kontakt = new GetConnection().kontakt(url);
+            Statement cursor = kontakt.createStatement();
+            ResultSet r = cursor.executeQuery(query);
+
+            // Lägger till resultatet från databasen i en arraylist och returnerar den
+            while (r.next()) {
+                String CustomerID = String.valueOf(r.getInt("CustomerID"));
+                String Name = r.getString("Name");
+                String Email = r.getString("Email");
+                String Phone = r.getString("Phone");
+
+                lst_response.add(CustomerID + "\t" + Name + "\t\t" + Email + "\t\t" + Phone);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return lst_response;
+    }
+
+    public ArrayList<String> selectAllBookingsSortedByDate() {
+        String query = "SELECT Bookings.BookingID, Bookings.Day, Bookings.Time, Tables.TableNumber, Customers.Name FROM Bookings"
+                + " JOIN Customers ON Bookings.CustomerID = Customers.CustomerID"
+                + " JOIN Tables ON Bookings.TableID = Tables.TableID"
+                + " ORDER BY Bookings.Day ASC, Bookings.Time ASC;";
+        ArrayList<String> lst_response = new ArrayList<String>();
+
+        try {
+            Connection kontakt = new GetConnection().kontakt(url);
+            Statement cursor = kontakt.createStatement();
+            ResultSet r = cursor.executeQuery(query);
+
+            // Lägger till resultatet från databasen i en arraylist och returnerar den
+            while (r.next()) {
+                String BookingID = String.valueOf(r.getInt("BookingID"));
+                String Day = r.getString("Day");
+                String Time = r.getString("Time");
+                String TableNumber = String.valueOf(r.getString("TableNumber"));
+                String Name = r.getString("Name");
+                lst_response.add(BookingID + "\t" + Day + "\t" + Time + "\t" + TableNumber + "\t" + Name);
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
