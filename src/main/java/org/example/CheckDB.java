466688fc05fb36cc00ad9b8e039c753fd24860e9
Jenny Storm <jenny.storm@gmail.com>
2024-01-17 17:01:09 +0100

    Modifierat CheckDB så den kollar om det finns bord lediga

--- src/main/java/org/example/CheckDB.java
+++ src/main/java/org/example/CheckDB.java
@@ -1,38 +1,48 @@
 package org.example;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class CheckDB {
     static String url = CreateDB.url;
+
     public static void main(String[] args) {
         // Test exempel
-        //System.out.println(CheckDB.CheckBooking("2024-01-18", "21:00-23:00"));
-        //System.out.println(CheckDB.CheckBooking("2030-01-18", "21:00-23:00"));
+        //System.out.println(CheckDB.CheckBooking("2024-01-27", "17:00-19:00"));
+        //System.out.println(CheckDB.CheckBooking("2024-01-28", "19:00-21:00"));
+
     }
-    public static String CheckBooking(String date, String time) {
-        String query = "SELECT Day, Time FROM Bookings WHERE Day = ? AND Time = ?";
+
+    public static Integer CheckBooking(String date, String time) {
+        String query = "SELECT Day, Time, TableID FROM Bookings WHERE Day = ? AND Time = ?";
+        ArrayList<Integer> response = new ArrayList<Integer>();
 
         try {
             Connection kontakt = new GetConnection().kontakt(url);
             PreparedStatement cursor = kontakt.prepareStatement(query);
-
             cursor.setString(1, date);
             cursor.setString(2, time);
+
             ResultSet r = cursor.executeQuery();
 
-            int rowCount = 0;
-            while (r.next()){
-                rowCount += 1;
+            // Hämtar TableID från de bokningar som är det aktuella datumet och tiden och sorterar dem i nummerordning
+            while (r.next()) {
+                Integer TableID = r.getInt("TableID");
+                response.add(TableID);
             }
-            if (rowCount == 0) {
-                return "Tiden är ledig";
-            } else {
-                return "Tiden är redan bokad";
-            }
+            Collections.sort(response);
 
-        } catch (SQLException e) {
+            // Kollar om listan innehåller 5 bokningar eller mindre, returnerar ledigt bordsnummer om det finns eller null
+            int size = response.size();
+            if (size > 4) {
+                return null;
+            } else {
+                return (size + 1);
+            }
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        return "Ran out of code";
+        return null;
     }
 }
--- src/main/java/org/example/InsertDB.java
+++ src/main/java/org/example/InsertDB.java
@@ -13,11 +13,16 @@ public class InsertDB {
         //add.InsertIntoTableTables(4);
         //add.InsertIntoTableTables(4);
         //add.InsertIntoTableTables(4);
-        add.InsertIntoTableBookings("Nalle Puh", "070 777 88 99", 3, 5,"2024-01-19", "17:00-19:00");
-        add.InsertIntoTableBookings("Robin Hood", "070 888 55 22",4, 3, "2024-01-19", "19:00-21:00");
-        add.InsertIntoTableBookings("Riddar Kato", "070 444 55 66", 2,1,"2024-01-18", "21:00-23:00");
-        add.InsertIntoTableBookings("Mumin Pappa", "070 222 55 88", 4, 4,"2024-01-21", "19:00-21:00");
-        add.InsertIntoTableBookings("Phileas Fogg", "070 333 55 77", 4, 2, "2024-01-20", "17:00-19:00");
+        add.InsertIntoTableBookings("Nalle Puh", "070 777 88 99", 3, 5,"2024-01-27", "17:00-19:00");
+        add.InsertIntoTableBookings("Robin Hood", "070 888 55 22",4, 1, "2024-01-28", "19:00-21:00");
+        add.InsertIntoTableBookings("Riddar Kato", "070 444 55 66", 2,1,"2024-01-26", "21:00-23:00");
+        add.InsertIntoTableBookings("Mumin Pappa", "070 222 55 88", 4, 1,"2024-02-02", "19:00-21:00");
+        add.InsertIntoTableBookings("Phileas Fogg", "070 333 55 77", 4, 1, "2024-02-03", "17:00-19:00");
+        add.InsertIntoTableBookings("Kulla Gulla", "070 987 65 21", 4, 2, "2024-01-27", "17:00-19:00");
+        add.InsertIntoTableBookings("Hubert Norlen", "070 666 25 25", 3, 1, "2024-01-27", "17:00-19:00");
+        add.InsertIntoTableBookings("Aslan", "070 147 58 69", 4, 3, "2024-01-27", "17:00-19:00");
+        add.InsertIntoTableBookings("Skorpan", "070 321 65 98", 2, 4, "2024-01-27", "17:00-19:00");
+        add.InsertIntoTableBookings("Törnrosa", "070 258 69 36", 2, 2, "2024-01-28", "19:00-21:00");
     }
 
     // Sätter in parametervärden i tabellen Tables
