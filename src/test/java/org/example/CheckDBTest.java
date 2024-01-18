16bc0d5f63519a3ceb3c0e435b7f017a586b2087
KevinWanisian <143587922+KevinWanisian@users.noreply.github.com>
2024-01-18 12:50:47 +0100

    Update CheckDBTest.java

--- src/test/java/org/example/CheckDBTest.java
+++ src/test/java/org/example/CheckDBTest.java
@@ -11,11 +11,11 @@ class CheckDBTest {
 
     @Test
     public void testCheckBooking() {
-        // Kontrollerar att metoden returnerar 5 om det finns 4 bokningar
+        // Kontrollerar att metoden returnerar 2 om det finns 2 bokningar
         int result = CheckDB.CheckBooking("2024-01-27", "17:00-19:00");
         assertEquals(2, result);
 
-        // Kontrollerar att metoden returnerar 1 om det finns 3 bokningar
+        
         result = CheckDB.CheckBooking("2024-01-27", "19:00-21:00");
         assertEquals(1, result);
 
