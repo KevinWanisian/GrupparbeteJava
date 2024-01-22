bfe4613c0f7d67aa8b11ad416e8a93062f246fb4
Martin Lindberg <Martinlindberg@live.se>
2024-01-22 17:07:43 +0100

    Skapade ett test för Restaurant samt uppdaterade Lade till några imports på ClearDBTestet, har även kommenterat ut classreservation från main eftersom den fanns på två ställen

--- src/test/java/org/example/ClearDBTest.java
+++ src/test/java/org/example/ClearDBTest.java
@@ -1,27 +1,26 @@
 package org.example;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ClearDBTest {
     private static final String url = "jdbc:sqlite:src/main/java/org/example/bookingsystem.db";
     private Connection connection;
     private ClearDB clearDB;
 
-    @Before
+    @BeforeEach
     public void setUp() throws SQLException {
         connection = GetConnection.kontakt(url);
         connection.setAutoCommit(false);
         clearDB = new ClearDB(connection);
     }
 
-    @After
+    @AfterEach
     public void tearDown() throws SQLException {
         // Rulla tillbaka transaktionen efter varje test
         connection.rollback();
